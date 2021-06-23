2e8efba23583ee7252ca218748b7f2d11663c415
MasFlam <masflam@masflam.com>
2021-06-23 17:06:10 +0200

    Change main.cpp to use for_each

--- main.cpp
+++ main.cpp
@@ -7,24 +7,17 @@ int main()
 {
 	auto asc = $.iota<5, 5>();
 	auto desc = $.iota<5, -10>();
-	std::cout << "asc: " <<
-		std::get<0>(asc) << ' ' <<
-		std::get<1>(asc) << ' ' <<
-		std::get<2>(asc) << ' ' <<
-		std::get<3>(asc) << ' ' <<
-		std::get<4>(asc) << std::endl;
-	std::cout << "desc: " <<
-		std::get<0>(desc) << ' ' <<
-		std::get<1>(desc) << ' ' <<
-		std::get<2>(desc) << ' ' <<
-		std::get<3>(desc) << ' ' <<
-		std::get<4>(desc) << ' ' <<
-		std::get<5>(desc) << ' ' <<
-		std::get<6>(desc) << ' ' <<
-		std::get<7>(desc) << ' ' <<
-		std::get<8>(desc) << ' ' <<
-		std::get<9>(desc) << std::endl;
+	
+	std::cout << "asc: ";
+	$.for_each(asc, [](auto x) { std::cout << x << ' '; });
+	std::cout << std::endl;
+	
+	std::cout << "desc: ";
+	$.for_each(desc, [](auto x) { std::cout << x << ' '; });
+	std::cout << std::endl;
+	
 	auto tup = $(1, 2, 3, 4, 5);
 	$.for_each(tup, [](auto x) { std::cout << 2*x << std::endl; });
+	
 	return 0;
 }
