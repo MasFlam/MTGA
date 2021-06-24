ebf021ad5c34b5772bbb55279d5be6600ca39885
MasFlam <masflam@masflam.com>
2021-06-24 18:42:16 +0200

    This code will never be pretty... I don't know what I'm trying to achieve here

--- mtga.hpp
+++ mtga.hpp
@@ -12,15 +12,14 @@
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 	GNU General Public License for more details.
 	
-	You should have received a copy of the GNU General Public License version 3.0
-	along with this library. If not, see https://www.gnu.org/licenses/gpl-3.0.html
+	You should have received a copy of the GNU General Public License
+	version 3.0 along with this library.
+	If not, see https://www.gnu.org/licenses/gpl-3.0.html
 */
 
 #include <tuple>
 #include <type_traits>
 #include <utility>
-
-// TODO: organize
 
 class $$ {
 	
@@ -31,12 +30,10 @@ class $$ {
 	
 	template<typename Itype, Itype S, Itype N, Itype... Is>
 	struct _iota_seq<Itype, S, std::integral_constant<Itype, N>, Is...>
-		: _iota_seq<Itype, S + 1, std::integral_constant<Itype, N - 1>, Is..., S>
-	{};
+		: _iota_seq<Itype, S + 1, std::integral_constant<Itype, N - 1>, Is..., S> {};
 	
 	template<typename Itype, Itype S, Itype... Is>
-	struct _iota_seq<Itype, S, std::integral_constant<Itype, 0>, Is...>
-	{
+	struct _iota_seq<Itype, S, std::integral_constant<Itype, 0>, Is...> {
 		typedef std::integer_sequence<Itype, Is...> iseq;
 	};
 	
@@ -45,12 +42,10 @@ class $$ {
 	
 	template<typename Itype, Itype S, Itype N, Itype... Is>
 	struct _iota_rseq<Itype, S, std::integral_constant<Itype, N>, Is...>
-		: _iota_rseq<Itype, S - 1, std::integral_constant<Itype, N - 1>, Is..., S>
-	{};
+		: _iota_rseq<Itype, S - 1, std::integral_constant<Itype, N - 1>, Is..., S> {};
 	
 	template<typename Itype, Itype S, Itype... Is>
-	struct _iota_rseq<Itype, S, std::integral_constant<Itype, 0>, Is...>
-	{
+	struct _iota_rseq<Itype, S, std::integral_constant<Itype, 0>, Is...> {
 		typedef std::integer_sequence<Itype, Is...> iseq;
 	};
 	
@@ -58,34 +53,29 @@ class $$ {
 	struct _iota;
 	
 	template<typename Itype, Itype S, Itype N>
-	struct _iota<Itype, true, S, N>
-	{
+	struct _iota<Itype, true, S, N> {
 		typedef typename _iota_seq<Itype, S, std::integral_constant<Itype, N>>::iseq seq;
 	};
 	
 	template<typename Itype, Itype S, Itype N>
-	struct _iota<Itype, false, S, N>
-	{
+	struct _iota<Itype, false, S, N> {
 		typedef typename _iota_rseq<Itype, S, std::integral_constant<Itype, -N>>::iseq seq;
 	};
 	
 	template<typename Itype, Itype... Is>
-	inline constexpr auto _iota_internal(std::integer_sequence<Itype, Is...>) const noexcept
-	{
+	inline constexpr auto _iota_internal(std::integer_sequence<Itype, Is...>) const {
 		return (*this)(Is...);
 	}
 	
 public:
 	
 	template<typename Itype, Itype S, Itype N>
-	inline constexpr auto iota() const noexcept
-	{
+	inline constexpr auto iota() const {
 		return _iota_internal(typename _iota<Itype, N >= 0, S, N>::seq());
 	}
 	
 	template<int S, int N>
-	inline constexpr auto iota() const noexcept
-	{
+	inline constexpr auto iota() const {
 		return iota<int, S, N>();
 	}
 	
@@ -94,29 +84,36 @@ public:
 private:
 	
 	template<typename Func, std::size_t I, std::size_t N, typename... Elements>
-	struct _for_each_call
-	{
-		inline constexpr auto _call(const std::tuple<Elements...>& tuple, const Func& func) const
-		{
+	struct _for_each_call {
+		inline constexpr auto _call(
+			const std::tuple<Elements...>& tuple,
+			const Func& func
+		) const {
 			func(std::get<I>(tuple));
 			_for_each_call<Func, I + 1, N - 1, Elements...>()._call(tuple, func);
 		}
 	};
 	
 	template<typename Func, std::size_t I, typename... Elements>
-	struct _for_each_call<Func, I, 0, Elements...>
-	{
-		inline constexpr auto _call(const std::tuple<Elements...>& tuple, const Func& func) const noexcept
-		{}
+	struct _for_each_call<Func, I, 0, Elements...> {
+		inline constexpr auto _call(
+			const std::tuple<Elements...>& tuple,
+			const Func& func
+		) const {}
 	};
 	
 public:
 	
 	template<typename Func, typename... Elements>
-	inline constexpr auto for_each(const std::tuple<Elements...>& tuple, const Func& func) const
-	{
-		_for_each_call<Func, 0, std::tuple_size<std::tuple<Elements...>>::value, Elements...>()
-			._call(tuple, func);
+	inline constexpr auto for_each(
+		const std::tuple<Elements...>& tuple,
+		const Func& func
+	) const {
+		_for_each_call<
+			Func, 0,
+			std::tuple_size<std::tuple<Elements...>>::value,
+			Elements...
+		>()._call(tuple, func);
 	}
 	
 	/**** map ****/
@@ -124,27 +121,27 @@ public:
 private:
 	
 	template<typename Func, std::size_t I, typename Tuple, typename... NewElements>
-	struct _map_internal
-	{
-		inline constexpr auto _do(const Tuple& tuple,
-		                          const Func& func,
-		                          const NewElements&... new_elements)
-		{
-			return _map_internal<Func, I - 1, Tuple,
-			                     decltype(func(std::get<I>(tuple))),
-			                     NewElements...>()._do(tuple, func,
-			                                           func(std::get<I>(tuple)),
-			                                           new_elements...);
+	struct _map_internal {
+		inline constexpr auto _do(
+			const Tuple& tuple,
+			const Func& func,
+			const NewElements&... new_elements
+		) const {
+			return _map_internal<
+				Func, I - 1, Tuple,
+				decltype(func(std::get<I>(tuple))),
+				NewElements...
+			>()._do(tuple, func, func(std::get<I>(tuple)), new_elements...);
 		}
 	};
 	
 	template<typename Func, typename Tuple, typename... NewElements>
-	struct _map_internal<Func, 0, Tuple, NewElements...>
-	{
-		inline constexpr auto _do(const Tuple& tuple,
-		                          const Func& func,
-		                          const NewElements&... new_elements)
-		{
+	struct _map_internal<Func, 0, Tuple, NewElements...> {
+		inline constexpr auto _do(
+			const Tuple& tuple,
+			const Func& func,
+			const NewElements&... new_elements
+		) const {
 			return std::make_tuple(func(std::get<0>(tuple)), new_elements...);
 		}
 	};
@@ -152,25 +149,30 @@ private:
 public:
 	
 	template<typename Func, typename... Elements>
-	inline constexpr auto map(const std::tuple<Elements...>& tuple, const Func& func) const
-	{
-		constexpr auto N = std::tuple_size<std::tuple<Elements...>>::value;
-		return _map_internal<Func, N - 1, std::tuple<Elements...>>()._do(tuple, func);
+	inline constexpr auto map(
+		const std::tuple<Elements...>& tuple,
+		const Func& func
+	) const {
+		return _map_internal<
+			Func,
+			std::tuple_size<std::tuple<Elements...>>::value - 1,
+			std::tuple<Elements...>
+		>()._do(tuple, func);
 	}
 	
 	/**** operator() ****/
 	
 	template<typename... Elements>
-	inline constexpr auto operator()(Elements&&... elements) const noexcept
-	{
+	inline constexpr auto operator()(Elements&&... elements) const {
 		return std::make_tuple(std::forward<Elements>(elements)...);
 	}
 	
 	/**** operator[] ****/
 	
 	template<typename... Elements>
-	inline constexpr std::size_t operator[](const std::tuple<Elements...>& tuple) const noexcept
-	{
+	inline constexpr std::size_t operator[](
+		const std::tuple<Elements...>& tuple
+	) const {
 		return std::tuple_size<std::tuple<Elements...>>::value;
 	}
 };
